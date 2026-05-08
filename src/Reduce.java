4b5cad2d2595f6fb5eef704293d28e68d9ef7903
Lucas K <l.doamaral@mail.utoronto.ca>
2026-05-07 20:06:57 -0400

    ...

---
+++ src/Reduce.java
@@ -0,0 +1,11 @@
+public class Reduce {
+    static void main() {
+        // Starting with a number n, if n is even divide it by 2. If n is odd, subtract 1. Repeat.
+        int steps = 0;
+        // start from 100 to 0
+        int start = 100:
+        // recursive function?
+
+        }
+    }
+}
