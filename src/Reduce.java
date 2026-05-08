bea9506772db38ff7c82729b20fc6680353b2315
Lucas K <l.doamaral@mail.utoronto.ca>
2026-05-08 13:38:28 -0400

    final

--- src/Reduce.java
+++ src/Reduce.java
@@ -3,9 +3,13 @@ public class Reduce {
         // Starting with a number n, if n is even divide it by 2. If n is odd, subtract 1. Repeat.
         int steps = 0;
         // start from 100 to 0
-        int start = 100:
-        // recursive function?
-
+        int start = 100;
+        while (0 < start) {
+            boolean isEven = start % 2 == 0;
+            if (isEven) {start = start/2;}
+            else {start -= 1;}
+            steps += 1;
         }
+        System.out.println(steps);
     }
 }
